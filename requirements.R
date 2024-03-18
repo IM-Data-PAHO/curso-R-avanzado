e1b8802cb0618ec441ad262ab845965d8ba0d94f
Rafael Leon <leonraf@paho.org>
2024-03-18 07:29:21 -0600

    Se carga el material del dia 1 del curso

---
+++ requirements.R
@@ -0,0 +1,33 @@
+# requirements.R ---------------------------------------------------------------
+# Descripción: Contiene el listado de paquetes necesarios para la correcta
+# ejecución de este proyecto.
+# Creado por -------------------------------------------------------------------
+# Nombre: CIM Data Team
+# Creado en: 2024-02-05
+# Editorial --------------------------------------------------------------------
+# Sección para notas o cambios editoriales.
+# ______________________________________________________________________________
+
+# cargar pacman si no lo está
+if (!require("pacman")) install.packages("pacman")
+
+# cargar paquetes necesarios
+pacman::p_load(
+  tidyr,
+  dplyr,
+  lubridate,
+  stringr,
+  rio,
+  ggplot2,
+  bench,
+  janitor,
+  # reclin2,
+  sf,
+  readr,
+  cleaner
+)
+
+
+
+
+
